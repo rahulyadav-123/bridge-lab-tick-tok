62889fb02c23a2871cdf45e76077300693272eea
rahul yadav <ry219613@gmail.com>
2021-10-04 11:06:41 +0530

    UC1 done

--- TicTokToe.java
+++ TicTokToe.java
@@ -1,6 +1,16 @@
 public class TicTokToe {
 	public static void main(String[] args) {
 		System.out.println("Helloworld, welcome to Tic tok game");
+		CreateBoard();
+
+
 	}
+	public static void CreateBoard() {
+		char board [] = new char [10];
+		for (int i=1; i<board; i++) {
+		board [i];
+	}
+  }
+
 
 }
